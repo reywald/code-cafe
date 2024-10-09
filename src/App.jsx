f21a144300f7675519d7f58d4897083041cfecab
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-10-09 19:12:46 +0100

    Rendering Items
    Render each item and its image as Thumbnail components in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import "./App.css";
 import Header from "./components/Header";
+import Thumbnail from "./components/Thumbnail";
+import { itemImages } from "./items";
+import { items } from "./items";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      {items.map((item) => (
+        <Thumbnail
+          key={item.itemId}
+          image={itemImages[item.imageId]}
+          title={item.title}
+        />
+      ))}
+    </>
+  );
 }
 
 export default App;
