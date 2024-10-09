e343a514f737cd243e0b6dc002c0e235a25a5122
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-10-09 18:48:49 +0100

    Thumbnail Component

---
+++ src/components/Thumbnail.jsx
@@ -0,0 +1,14 @@
+import "./Thumbnail.css";
+
+function Thumbnail({ image, title }) {
+  return (
+    <a href="#todo" className="thumbnail-component">
+      <div>
+        <img src={image} alt={title} />
+      </div>
+      <p>{title}</p>
+    </a>
+  );
+}
+
+export default Thumbnail;
