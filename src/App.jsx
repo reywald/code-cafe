1514af47a2705aa8a6e513504bf59fdbe916ab9b
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-10-11 00:42:24 +0100

    Interacting with a Server
    Add the axios library, add a proxy setting in vite's config, import and use useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 import Header from "./components/Header";
 import Home from "./components/Home";
-import { items } from "./items";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/items")
+      .then((result) => setItems(result.data))
+      .catch(console.error);
+  }, []);
+
   return (
     <>
       <Header title="Code Café" />
